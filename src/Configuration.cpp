e72355c93f31966f53653e4ab69cfd3dcb0d6c5c
jaisor <jaisor@gmail.com>
2021-03-21 17:22:41 -0700

    work on clock added text and digital

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -45,6 +45,12 @@ void EEPROM_loadConfig() {
     }
   }
 #endif
+
+  // FIXME: Always default NTP values
+  strcpy(configuration.ntpServer, NTP_SERVER);
+  configuration.gmtOffset_sec = NTP_GMT_OFFSET_SEC;
+  configuration.daylightOffset_sec = NTP_DAYLIGHT_OFFSET_SEC;
+
 }
 
 void EEPROM_wipe() {
