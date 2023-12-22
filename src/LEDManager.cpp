7c4462c93a207bf6731a74542492228a1730de5f
Jordan Marinov <jaisor@gmail.com>
2023-12-22 10:17:25 -0800

    Configs templates for different devices and few small tweaks

--- src/LEDManager.cpp
+++ src/LEDManager.cpp
@@ -2,7 +2,7 @@
 #include "LEDManager.h"
 #include "Configuration.h"
 
-CLEDManager::CLEDManager(const uint16_t size, float brigthness)
+CLEDManager::CLEDManager(const uint16_t size, float brightness)
 : size(size) {
-    this->brightness = brigthness;
+    this->brightness = brightness;
 }
